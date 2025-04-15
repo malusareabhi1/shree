0651b54b411a886e50eb71b8a22b098e22f541fb
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-15 17:45:53 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
+import streamlit as st
 
+st.title("👋 Hello, Streamlit!")
 
-import yfinance as yf
-import pandas as pd
+st.write("This is your first Streamlit app.")
+st.write("Streamlit makes it easy to build web apps with Python!")
 
-# Define stock symbol for Tata Motors (NSE)
-symbol = "TATAMOTORS.NS"
-
-# Download last 30 days data at 5-minute interval
-df = yf.download(tickers=symbol, period="30d", interval="5m")
-
-# Reset index and save to CSV
-df.reset_index(inplace=True)
-df.to_csv("tatamotors_5min_last_30days.csv", index=False)
-
-from pathlib import Path  
-filepath = Path('C:/Users/abhi/Documents/Python_ALGo_BOT/tatamotors_5min_last_30days.csv')  
-filepath.parent.mkdir(parents=True, exist_ok=True)  
-df.to_csv(filepath) 
-
-
-print("✅ Data downloaded and saved as 'tatamotors_5min_last_30days.csv'")
+name = st.text_input("What's your name?")
+if name:
+    st.success(f"Nice to meet you, {name}!")
