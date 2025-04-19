fbfd91c011fb4688b88c4a90def572f42f168168
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-19 15:01:36 +0530

    Update tradingview.py

--- tradingview.py
+++ tradingview.py
@@ -28,7 +28,7 @@ symbol_map = {
 
 selected_symbol = st.selectbox("📌 Select Market/Stock", options=list(symbol_map.keys()))
 #chart_url = f"https://www.tradingview.com/chart/?symbol={symbol_map[selected_symbol]}"
-chart_url = f"https://www.tradingview.com/widgetembed/?frameElementId=tradingview_live&symbol={symbol_map[selected_symbol]}&interval=1&theme=dark&style=1&locale=en"
+chart_url = f"https://www.tradingview.com/widgetembed/?frameElementId=tradingview_live&symbol={symbol_map[selected_symbol]}&theme=dark&style=1&locale=en"
 
 components.html(f"<iframe src='{chart_url}' width='100%' height='500' frameborder='0'></iframe>", height=500)
 
