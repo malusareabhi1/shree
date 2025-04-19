3074f2e7864aefc009f48be11e8f99729443e1ec
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-19 14:53:29 +0530

    Update tradingview.py

--- tradingview.py
+++ tradingview.py
@@ -18,3 +18,16 @@ tradingview_chart = """
 
 # Display in Streamlit
 components.html(tradingview_chart, height=500)
+
+symbol_map = {
+    "NIFTY 50": "NSE:NIFTY",
+    "BANK NIFTY": "NSE:BANKNIFTY",
+    "SENSEX (BSE)": "BSE:SENSEX",
+    "RELIANCE": "NSE:RELIANCE"
+}
+
+selected_symbol = st.selectbox("📌 Select Market/Stock", options=list(symbol_map.keys()))
+chart_url = f"https://www.tradingview.com/chart/?symbol={symbol_map[selected_symbol]}"
+
+components.html(f"<iframe src='{chart_url}' width='100%' height='500' frameborder='0'></iframe>", height=500)
+
