8f8c0ea826fc6a96b4a80a15d5b11c6f157300b6
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-19 14:25:37 +0530

    Update bot2.py

--- bot2.py
+++ bot2.py
@@ -60,7 +60,7 @@ if selected == "Dashboard":
     # Embed TradingView Ticker Tape widget
     ticker_embed_code = """
     <iframe src="https://www.tradingview.com/widgetembed/?frameElementId=tradingview_e9ab9&symbol=NSE%3ANIFTY" 
-    width="100%" height="100" style="border: none;"></iframe>
+    width="100%" height="50%" style="border: none;"></iframe>
    """
     #%2CNSE%3ABANKNIFTY
      # Display the ticker in the main area
