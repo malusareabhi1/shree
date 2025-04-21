fb0dd413a2f86d89b4c4bb245be493efced3d75d
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-21 12:38:55 +0530

    Update niftylive.py

--- niftylive.py
+++ niftylive.py
@@ -3,7 +3,7 @@ import yfinance as yf
 import plotly.graph_objects as go
 
 # ✅ This MUST be the first Streamlit command
-st.set_page_config(page_title="📊 NIFTY 50 Candlestick Chart", layout="wide")
+#st.set_page_config(page_title="📊 NIFTY 50 Candlestick Chart", layout="wide")
 
 # Now it's safe to use other Streamlit commands
 st.title("📈 NIFTY 50 Live Candlestick Chart")
