29c58ded7eec891819ac3e070ee50a87d673712c
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-22 11:15:08 +0530

    Update telgram.py

--- telgram.py
+++ telgram.py
@@ -35,7 +35,7 @@ def send_telegram_message(message):
 ist = pytz.timezone("Asia/Kolkata")
 
 now = datetime.datetime.now(ist)
-now = datetime.now().strftime("%H:%M:%S")
+now = datetime.datetime.now().strftime("%H:%M:%S")
 # Check current time
 #now = datetime.datetime.now().time()
 today = datetime.datetime.today().weekday()  # Monday = 0, Sunday = 6
