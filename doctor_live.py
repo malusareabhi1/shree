c176278258f281f4b3a19dade5accd69779672c0
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-22 10:20:10 +0530

    Update doctor_live.py

--- doctor_live.py
+++ doctor_live.py
@@ -139,8 +139,9 @@ if run:
     ist = pytz.timezone("Asia/Kolkata")
     
     # Get current IST datetime
+    now_ist = datetime.now(ist).time()  # Get current time in IST
     #now_ist = datetime.now(ist)
-    now_ist = datetime.datetime.now(ist)
+    #now_ist = datetime.datetime.now(ist)
     # Option A: Format with strftime for time only
     time_str = now_ist.strftime("%H:%M:%S")
     st.write(f"⏰ Current IST Time: {time_str}")
