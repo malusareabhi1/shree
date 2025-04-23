379ded03548450ca6553b4c4ab7ce5c792cf34db
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-23 16:14:44 +0530

    Update algobot3.py

--- algobot3.py
+++ algobot3.py
@@ -1319,12 +1319,17 @@ elif selected == "Live Algo Trading":
     # ─── MARKET OPEN/CLOSE MESSAGE ────────────────────────────────────────────────
     now = latest.name
     if now.hour == 9 and now.minute == 15:
-        market_msg = "📈 Market Opened at 09:15"
+        market_msg = "📈 Market Opened at 09:15 But My Doctor Stratergy will Start 09:30 "
         st.success(market_msg)
         send_telegram(market_msg)
+
+     if now.hour == 14 and now.minute == 30:
+        market_close_msg = "📉 Doctor Stratergy will  not take Trade after 02:30"
+        st.warning(market_close_msg)
+        send_telegram(market_close_msg)
     
     if now.hour == 15 and now.minute == 30:
-        market_close_msg = "📉 Market Closed at 15:30"
+        market_close_msg = "📉 Market Closed at 15:30 Bye ! See you Tomorrow 9:30"
         st.warning(market_close_msg)
         send_telegram(market_close_msg)
     
