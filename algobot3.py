e53cc11b8dee9ea13bc168f2fa7588cd7efdf0f2
Abhi Malusare <malusareabhi1@gmail.com>
2025-04-28 11:17:13 +0530

    Update algobot3.py

--- algobot3.py
+++ algobot3.py
@@ -1519,8 +1519,9 @@ elif selected == "Live Algo Trading":
     @st.cache_data(ttl=60)
     def fetch_data(ticker: str) -> pd.DataFrame:
         df = yf.download(ticker, interval="5m", period="5d", progress=False)
+         st.write("Columns after read:", df.columns.tolist())
         df.index = pd.to_datetime(df.index)
-        st.write("Columns after read:", df.columns.tolist())
+       
         if df.index.tz is None:
             df = df.tz_localize("UTC").tz_convert("Asia/Kolkata")
         else:
