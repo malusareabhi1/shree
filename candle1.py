58474f801cd6f6261e9a3fe9f065409e56e95d8f
Abhi Malusare <malusareabhi1@gmail.com>
2025-05-05 12:05:29 +0530

    Update candle1.py

--- candle1.py
+++ candle1.py
@@ -80,7 +80,7 @@ def plot_candles_with_sma(df):
 
 symbol = "^NSEI"
 df = fetch_5min_data(symbol)
-st.write(df.head(5))
+st.write(df.head(25))
 trend = get_trend(df)
 col1, col2, col3, col4 = st.columns(4)
 col1.metric("📈 Trend", trend)
