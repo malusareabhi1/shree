bfd5453ac47f82c37f1b17dda1693541f40fac8b
Abhi Malusare <malusareabhi1@gmail.com>
2025-05-07 14:23:04 +0530

    Update doctor_live_signal_log.py

--- doctor_live_signal_log.py
+++ doctor_live_signal_log.py
@@ -190,6 +190,7 @@ if __name__ == "__main__":
     df = get_nifty_data()
     df.rename(columns={df.columns[0]: "Date"}, inplace=True)
     st.write("DATA")
+    st.dataframe(df.head(5), use_container_width=True, hide_index=True)
     st.write(df.head(5))
     if df.empty:
         st.warning("No data available for today’s 5‑min bars.")
