6e059e34bbae53b01cadd4e9070af72807557e7c
Abhi Malusare <malusareabhi1@gmail.com>
2025-05-08 13:25:42 +0530

    Update algo_dash.py

--- algo_dash.py
+++ algo_dash.py
@@ -84,10 +84,10 @@ if section == "Live Trading":
 
     try:
         data = yf.download(tickers=ticker, period="1d", interval="1m", progress=False)
-        if isinstance(df.columns, pd.MultiIndex):  # This checks if the columns are a MultiIndex
-            df.columns = df.columns.get_level_values(0)
+        if isinstance(data.columns, pd.MultiIndex):  # This checks if the columns are a MultiIndex
+            data.columns = data.columns.get_level_values(0)
                  # Ensure datetime index is timezone-aware in UTC and then convert to IST
-            df.index = df.index.tz_convert("Asia/Kolkata")
+            data.index = data.index.tz_convert("Asia/Kolkata")
 
         
         if not data.empty:
