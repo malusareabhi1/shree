38880f616e9a82b19d9e798028c70b670da589be
Abhi Malusare <malusareabhi1@gmail.com>
2025-05-08 14:27:13 +0530

    Update algo_dash.py

--- algo_dash.py
+++ algo_dash.py
@@ -35,7 +35,7 @@ chat_id= os.getenv("TELEGRAM_CHAT_ID")  # optionally, use .env for chat_id too
 def send_telegram_message(message):
     url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
     payload = {
-        "chat_id": TELEGRAM_CHAT_ID,
+        "chat_id": CHAT_ID,
         "text": message,
         "parse_mode": "Markdown"
     }
