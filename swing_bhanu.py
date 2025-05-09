6bbd16cf989cca10b3e3673ebfeff1d8318d14d8
Abhi Malusare <malusareabhi1@gmail.com>
2025-05-09 17:26:44 +0530

    Update swing_bhanu.py

--- swing_bhanu.py
+++ swing_bhanu.py
@@ -2,7 +2,7 @@ import yfinance as yf
 import pandas as pd
 import streamlit as st
 import plotly.graph_objects as go
-
+import numpy as np
 def scan_bhanushali_strategy(stock):
     # Download historical stock data
     df = yf.download(stock, period='90d', interval='1d')
