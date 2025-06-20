a5f030cf5da03d573258b3f17f418a91ec2e9a49
Abhi Malusare <malusareabhi1@gmail.com>
2025-06-20 21:49:33 +0530

    Update threepm.py

--- threepm.py
+++ threepm.py
@@ -8,7 +8,7 @@ st.title("📈 NIFTY 3PM Breakout with 100-Point Move")
 
 symbol = "^NSEI"  # Nifty 50 Index
 interval = "15m"
-points_required = 50
+points_required = 10
 
 st.write("Fetching last 30 days of 15-minute NIFTY data...")
 data = yf.download(symbol, period="30d", interval=interval)
